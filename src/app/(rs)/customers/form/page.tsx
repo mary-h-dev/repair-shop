27d89c6b25529812250f40f1d7000305651fd9d0
maryamHabibpourr <Einesheikh_12@yahoo.com>
2025-03-26 20:04:10 +0000

    Refactor installation instructions, add SQL queries for database operations, and revert package.json dev script to standard configuration

---
+++ src/app/(rs)/customers/form/page.tsx
@@ -0,0 +1,59 @@
+import { getCustomer } from "@/lib/queries/getCustomer";
+import { BackButton } from "@/components/BackButton";
+import * as Sentry from "@sentry/nextjs"
+// import CustomerForm from "@/app/(rs)/customers/form/CustomerForm";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export async function generateMetadata({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | undefined }>
+}) {
+    const { customerId } = await searchParams
+
+    if (!customerId) return { title: "New Customer" }
+
+    return { title: `Edit Customer #${customerId}` }
+}
+
+
+
+export default async function CustomerFormPage({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | undefined }>
+}) {
+    try {
+        // const { getPermission } = getKindeServerSession()
+        // const managerPermission = await getPermission("manager")
+        // const isManager = managerPermission?.isGranted
+
+        const { customerId } = await searchParams
+
+        // Edit customer form 
+        if (customerId) {
+            const customer = await getCustomer(parseInt(customerId))
+
+            if (!customer) {
+                return (
+                    <>
+                        <h2 className="text-2xl mb-2">Customer ID #{customerId} not found</h2>
+                        <BackButton title="Go Back" variant="default" />
+                    </>
+                )
+            }
+            console.log(customer)
+            // put customer form component 
+            // return <CustomerForm key={customerId} isManager={isManager} customer={customer} />
+        } else {
+            // new customer form component 
+            // return <CustomerForm key="new" isManager={isManager} />
+        }
+
+    } catch (e) {
+        if (e instanceof Error) {
+            Sentry.captureException(e)
+            throw e
+        }
+    }
+}
