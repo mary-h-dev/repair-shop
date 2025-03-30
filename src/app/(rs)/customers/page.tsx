d79ff23300430c0c488ea5db86ed52a64778205b
maryamHabibpourr <Einesheikh_12@yahoo.com>
2025-03-30 12:34:02 +0000

    Refactor Customers component to support async data fetching and integrate CustomerSearch. Update metadata for improved page title. Prepare for future implementation of CustomerTable for displaying search results.

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import CustomerSearch from "@/app/(rs)/customers/CustomerSearch"
+import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults"
+// import CustomerTable from "@/app/(rs)/customers/CustomerTable"
 
-const Customers = () => {
-  return (
-    <div>
-      customers
-    </div>
-  )
+export const metadata = {
+    title: "Customer Search",
 }
 
-export default Customers
+export default async function Customers({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | undefined }>
+}) {
+    const { searchText } = await searchParams
+
+    if (!searchText) return <CustomerSearch />
+
+    const results = await getCustomerSearchResults(searchText)
+
+    return (
+        <>
+            <CustomerSearch />
+            {/* {results.length ? <CustomerTable data={results} /> : (
+                <p className="mt-4">No results found</p>
+            )} */}
+        </>
+    )
+}
