1d52585a76fbba080be6017bff4329c25ea40ac3
maryamHabibpourr <Einesheikh_12@yahoo.com>
2025-03-31 20:04:59 +0000

    Update package dependencies: bump drizzle-zod to version 0.7.1 and drizzle-kit to version 0.30.6. Refactor environment variable configuration in db/index.ts for better clarity and maintainability. Remove unused Home component file.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,11 @@
 import { drizzle } from "drizzle-orm/neon-http"
 import { neon } from "@neondatabase/serverless"
 import { config } from "dotenv"
-config({ path: ".env.local" })
+// config({ path: ".env.local" })
 
-// if (process.env.NODE_ENV === "development") {
-//     config({ path: ".env.local" })
-// }
+if (process.env.NODE_ENV === "development") {
+    config({ path: ".env.local" })
+}
 
 const sql = neon(process.env.DATABASE_URL!)
 
